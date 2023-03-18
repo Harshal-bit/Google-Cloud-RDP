82928cec7b729d96102d521b2bcf778876a933fc
Harshal-bit <83230717+Harshal-bit@users.noreply.github.com>
2023-03-18 14:27:57 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-printf "Installing RDP Be Patience..." >&2
+printf "Installing RDP Be Patient..." >&2
 {
-sudo useradd -m NAHID
-sudo adduser NAHID sudo
-echo 'NAHID:1234' | sudo chpasswd
+sudo useradd -m root
+sudo adduser root sudo
+echo 'root:root' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -19,16 +19,16 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y
-sudo apt -y install obs-studio
+
 sudo apt -y install firefox
-sudo apt -y install qbittorrent
+
 sudo apt install nload
-sudo adduser NAHID chrome-remote-desktop
+sudo adduser root chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - NAHID -c """$CRP"""
+su - root -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
-printf 'Your SUDO Pasword is: 1234 \n'
+printf 'Your SUDO Pasword is: root \n'
